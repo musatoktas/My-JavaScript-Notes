fd4c6e9579e634b4ef66d41aa0acf041b0bd10cf
Musa Toktas <musatoktas@users.noreply.github.com>
2018-10-12 16:32:38 +0300

    Create Arrays and For Loop.js

---
+++ Arrays and For Loop.js
@@ -0,0 +1,38 @@
+// Arrays
+// arrays
+// var array = new Array();
+// array[0] = "Musa";
+// array[1] = 2;
+// array[2] = function (name) {
+// 	console.log("hello" + name);
+// };
+// array[3] = {jobs: " siemens, full stack development, nothin"};
+
+// console.log(array);
+// var names = ["musa", "harun", "rumeysa", "zahide"];
+
+// for (var i = 0; i < names.length; i++) {
+// 	console.log("hola " + names[i] + "!");
+// };
+// for (var i = 0; i < names.length; i++) {
+//   console.log("Hello " + names[i]);
+// }
+
+// var names2 = ["mis","jack", "joe"];
+
+// var myObj = {
+// 	name: "Musa",
+// 	company: "siemens",
+// 	department: "digitalization"
+// }
+
+// for (var prop in myObj) {
+// 	console.log(prop + ": " + myObj[prop]);
+// 
+
+var counter = 0;
+var myArray = ["Yaakov", 2, {handle: "yaakovchaikin"}];
+for (var i = 0; i < myArray.length; i++) {
+  counter++;
+}
+console.log(counter);
