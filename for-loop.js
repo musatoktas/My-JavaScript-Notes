4bd11aa1acbd180d638df987ef3e48f4946ff6dd
Musa Toktas <musatoktas@users.noreply.github.com>
2022-01-13 16:14:37 +0300

    Create for-loop.js

---
+++ for-loop.js
@@ -0,0 +1,37 @@
+const myArray = [2, 3, 4, 5, 6];
+/*
+for (let i = 1; i < 10; i += 2) {
+  myArray.push(i);
+}
+
+for (let i = 9; i>0; i -= 2 ){
+  myArray.push(i);
+}
+
+for (let i = 0; i < myArray.length; i++) {
+   console.log(myArray[i]);
+}
+
+console.log(myArray);
+
+
+-- multiplier function
+function multiplyAll(arr) {
+  var product = 1;
+  // Only change code below this line
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < arr[i].length; j++) {
+      product = product * arr[i][j];
+    }
+    console.log(product);
+
+  }
+
+  // Only change code above this line
+  return product;
+}
+
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
+--- multiplier ends--
+
+*/
