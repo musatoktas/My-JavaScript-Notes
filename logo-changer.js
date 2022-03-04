c0e201e7049cc5b71c770089981b255d36cd8754
Musa Toktas <musatoktas@users.noreply.github.com>
2022-03-04 16:28:35 +0300

    Create logo-changer.js

---
+++ logo-changer.js
@@ -0,0 +1,10 @@
+
+<script>
+function logoDegistir() {
+  var x = document.getElementsByClassName("logo");
+  x[0].innerHTML = "<a href='https://demircarrental.com/' class='navbar-brand py-0'><img src='http://demircarrental.com/wp-content/uploads/2021/11/DEMİRCAR-rental-logo-1.png' alt='Demir Car Rental'></a>";
+  var y = document.getElementsByClassName("d-md-block");     
+  y[0].innerHTML = "<a href='https://demircarrental.com/' class='navbar-brand py-0'><img src='http://demircarrental.com/wp-content/uploads/2021/11/DEMİRCAR-rental-logo-beyaz.png' alt='Demir Car Rental'></a>";                      
+                        }
+                        
+</script>
